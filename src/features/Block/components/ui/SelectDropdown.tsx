8462dc1e1d60cf848a8e9f26404bf96dc37dad8a
Seongmin0218 <yyggy365@gmail.com>
2026-08-13 10:20:11 +0900

    7

--- src/features/Block/components/ui/SelectDropdown.tsx
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps } from 'react'
-import { Select } from '../../../../components/ui/Select'
-
-export type SelectDropdownProps = ComponentProps<typeof Select>
-
-export function SelectDropdown(props: SelectDropdownProps) {
-  return <Select {...props} />
-}
--- src/features/studio/components/palette/index.ts
+++
@@ -1,3 +0,0 @@
-export {
-  StudioDraggableBlock,
-} from './StudioDraggableBlock'
